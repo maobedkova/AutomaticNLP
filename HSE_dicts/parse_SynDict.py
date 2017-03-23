d00657f21700c5168498897d73d44e20f981acf1
Maria Obedkova <maryobedkova@gmail.com>
2017-03-23 20:55:48 +0300

    update according TEI scheme

--- HSE_dicts/parse_SynDict.py
+++ HSE_dicts/parse_SynDict.py
@@ -3,64 +3,46 @@
 import re
 
 f = open('abr2w.txt', 'r', encoding='utf-8')
-w = open('test.xml', 'w', encoding='utf-8')
+w = open('synonyms.tei', 'w', encoding='utf-8')
+t = open('test_synonyms.txt', 'w', encoding='utf-8')
 
 body = ''
+num = 1
 
 for line in f:
-    # w.write(line + '\n')
     print (line)
+    t.write(line)
 
     ### REFERENCE
     reference = re.findall('[Сс]м\. ?([-\w\(\) ,.]+?) ?[.\n\|;]', line)
     ref_body = ''
     for ref in reference:
         refs = re.split(', ?', ref)
-        ref_body = '<xr type="syn">\n<lbl>Смотри синоним</lbl>\n'
-        i = 1
+        ref_body = '<xr type="synonym">\n<lbl>Смотри синоним</lbl>\n'
         for ref in refs:
-            ref_body += '<ref n="' + str(i) + '">\n' + ref + '</ref>\n'
-            i += 1
+            ref_body += '<ref>' + ref + '</ref>\n'
         ref_body += '</xr>\n'
-
-    # w.write('R' + ref_body + '\n')
-    print('REF', refs)
-
     new_line = re.sub('([Сс]м\. ?[-\w\(\) ,.]+?) ?(?:[.\n\|;])', '', line)
 
-
-    ### DEFENITION
-    defenition1 = re.findall('\[(\w{2,3}\.)?(.+?)(?:\((\w{2,3}\.)\))?\]', new_line)
-    def_body = ''
-    for defen in defenition1:
-        def_body += '<def>\n<text>\n'+defen[1]+'<text>\n<lang lang_code="" source="">\n'+defen[0]+'</lang>\n</def>\n'
-    new_line = re.sub('\[(\w{2,3}\.)?.+?(?:\(\w{2,3}\.\))?\]', '', new_line)
-
-    defenition2 = re.findall('\[(.+?)\((.+?)\)\]', new_line)
-    for defen in defenition2:
-        def_body += '<def>\n<text>\n'+defen[0]+'</text>\n<source n="1" source_code="">\n'+defen[1]+'</source>\n</def>\n'
-    new_line = re.sub('\[.+?\(.+?\)\]', '', new_line)
-
-    # w.write('D' + def_body + '\n')
-    print ('DEF', def_body)
+    if reference:
+        print('REF', reference)
+        t.write('REF ' + str(reference) + '\n')
 
 
     ### COMPARISON
-    comparison = re.findall('[Сс]р\. ?<(.+?)>', new_line)
+    comparison = re.findall('\[?[Сс]р\. ?<?([^><\]\[]+)>?\]?', new_line)
     comp_body = ''
     for c in comparison:
         comps = re.split(' и |, ?', c)
-        comp_body = '<xr type="syn">\n<lbl>Сравни с</lbl>\n'
-        i = 1
+        comp_body = '<xr type="synonym">\n<lbl>Сравни с</lbl>\n'
         for comp in comps:
-            comp_body += '<ref n="'+str(i)+'">\n'+comp+'</ref>\n'
-            i += 1
+            comp_body += '<ref>'+comp+'</ref>\n'
         comp_body += '</xr>\n'
+    new_line = re.sub('\[?[Сс]р\. ?<?([^><\]\[]+)>?\]?', '', new_line)
 
-        # w.write('C ' + comp_body + '\n')
-        print('COMP', comps)
-
-    new_line = re.sub('[Сс]р\. ?<.+?>', '', new_line)
+    if comparison:
+        print('COMP', comparison)
+        t.write('COMP ' + str(comparison) + '\n')
 
 
     ### ANTONYM
@@ -68,70 +50,103 @@ for line in f:
     ant_body = ''
     for a in antonym:
         ants = re.split(' и |, ?', a)
-        ant_body = '<xr type="ant">\n<lbl>\nПротивопоставление с</lbl>\n'
-        i = 1
+        ant_body = '<xr type="antonym">\n<lbl>\nПротивопоставление с</lbl>\n'
         for ant in ants:
-            ant_body += '<ref n="' + str(i) + '">\n' + ant + '</ref>\n'
-            i += 1
+            ant_body += '<ref>' + re.sub('[><]', '', ant) + '</ref>\n'
         ant_body += '</xr>\n'
-
-        # w.write('A' + ant_body + '\n')
-        print('ANT', antonym)
-
     new_line = re.sub('[Пп]рот\. ?<.+?>', '', new_line)
+
+    if antonym:
+        print('ANT', antonym)
+        t.write('ANT ' + str(antonym) + '\n')
+
+
+    ### DEFENITION
+    defenition = re.findall('\[(.+?)(?:\(([\w., ]+?)\))?\]', new_line)
+    def_body= ''
+    for defen in defenition:
+        def_body += '<def>\n<text>'+re.sub('[><]', '', defen[0])+\
+                    '</text>\n<ref>'+defen[1]+'</ref>\n</def>\n'
+    new_line = re.sub('\[(.+?)(?:\(([\w., ]+?)\))?\]', '', new_line)
+
+    if defenition:
+        print ('DEF', defenition)
+        t.write('DEF ' + str(defenition) + '\n')
 
 
     ### IDIOM
     idioms = re.findall('\|+ ?([-\w\(\) ,.?!;]+?) ?(?:\n|[Сс]м\.)', new_line)
+    new_line = re.sub('\|+ ?[-\w\(\) ,.?!;]+? ?(?:\n|[Сс]м\.)', '', new_line)
     id_body = ''
     for idiom in idioms:
         ids = re.split(', ?', idiom)
-        id_body = ''
-        i = 1
         for id in ids:
-            id_body += '<re type="idiom">\n<quote>\n'+id+'</quote>\n</re>\n'
-            i += 1
+            id_body += '<text>' + re.sub('[><]', '', id) + '</text>\n'
+    if id_body != '':
+        id_body = '<cit type="colloc">\n<form><usg type="style">Идиома</usg></form>' + id_body + '</cit>\n'
 
-        # w.write('I' + id_body + '\n')
-        print('ID', ids)
+    if idioms:
+        print('ID', idioms)
+        t.write('ID ' + str(idioms) + '\n')
 
-    new_line = re.sub('\|+ ?[-\w\(\) ,.?!;]+? ?(?:\n|[Сс]м\.)', '', new_line)
+
+    ### FORM
+    forms1 = re.findall('^[-\w .!]+\(([-\w .,]+?)\)', new_line)
+    new_line = re.sub('\([-\w .,]+?\)', '', new_line)
+    forms2 = re.findall('((?:Действ|Страд)\. форма): <(.+?)>', new_line)
+    new_line = re.sub('((?:Действ|Страд)\. форма): <(.+?)>', '', new_line)
+    forms = forms1 + forms2
+    sex_body = ''
+    asp_body = ''
+    part_body = ''
+    form_body = ''
+    for f in forms1:
+        if re.search('^\w\. \w\. .+', f):
+            sex_body += '<cit type="gender_parallel">'+f+'</cit>\n'
+        elif re.search('^-|-$', f):
+            part_body += '<orth extent="part">'+f+'</orth>\n'
+        else:
+            part_body += '<orth>'+f+'</orth>\n'
+
+    for f in forms2:
+        asp_body += '<orth>' + f[1] + '</orth>\n<note>' +f[0]+ '</note>\n'
+
+    if asp_body != '':
+        asp_body = '<inflection>\n' + asp_body + '</inflection>\n'
+    if part_body != '':
+        part_body = '<inflection>\n' + part_body + '</inflection>\n'
+    if asp_body != '' or part_body != '':
+        form_body += '<form type="inflected">\n' + asp_body + part_body + '</form>\n'
+
+    if forms:
+        print('FOR', forms)
+        t.write('FOR ' + str(forms) + '\n')
 
 
     ### EXAMPLE
     examples = []
-    examples1 = re.findall('// ?([^\">]+?)\. ?(.+?)\.', new_line)
+    examples1 = re.findall('\. ?([А-Я][^.]+?(?:\.\.\.)?)[\">]\.? ?(.+?)\.', new_line)
+    new_line = re.sub('\. ?([А-Я][^.]+?(\.\.\.)?)[\">]\.? ?(.+?)\.', '', new_line)
+    examples2 = re.findall('// ?([^\">]+?)\. ?(.+?)\.', new_line)
     new_line = re.sub('// ?[^\">]+?\. ?.+?\.', '', new_line)
-    examples2 = re.findall('[\"<](.+?)[\">]\.? ?(.+?)\.', new_line)
+    examples3 = re.findall('[\"<](.+?)[\">]\.? ?(.+?)\.', new_line)
     new_line = re.sub('[\"<](.+?)[\">]\.? ?(.+?)\.', '', new_line)
-    examples3 = re.findall('\. ?([А-Я].+?)[\">]\.? ?(.+?)\.', new_line)
-    new_line = re.sub('\. ?([А-Я].+?)[\">]\.? ?(.+?)\.', '', new_line)
     examples += examples1 + examples2 + examples3
     ex_body = ''
     for ex in examples:
-        ex_body += '<cit type="example">\n<quote>\n'+ex[0]+'</quote>\n<author>\n'+ex[1]+'</author>\n</cit>\n'
+        if re.search('\w', ex[0]):
+            ex_body += '<cit type="example">\n<text>'+re.sub('[><]', '', ex[0])+\
+                       '</text>\n<ref>'+re.sub('[><]', '', ex[1])+'</ref>\n</cit>\n'
+        else:
+            ex_body += '<cit type="example">\n<text>' + re.sub('[><]', '', ex[0]) + '</text>\n</cit>\n'
     examples4 = re.findall('<(.+?)>\.', new_line)
     new_line = re.sub('<(.+?)>\.', '', new_line)
     if examples4:
-        ex_body += '<cit type="example">\n<quote>\n'+examples4[0]+'</quote>\n</cit>\n'
+        ex_body += '<cit type="example">\n<text>'+re.sub('[><]', '', examples4[0])+'</text>\n</cit>\n'
 
-    # w.write('EX' + ex_body + '\n')
-    print ('EX', examples)
-
-
-    ### FORM
-    forms = re.findall('^[-\w .!]+\(([-\w .,]+?)\)', new_line)
-    sex_body = ''
-    form_body = ''
-    for f in forms:
-        sex_body += '<sex_parall>\n'+ ', '.join(forms)+'</sex_parall>\n'
-        form_body += '<form type="inflected">\n<orth>\n'+ ', '.join(forms)+'</orth>\n</form>\n'
-
-        # w.write('F ' + sex_body + '\n')
-        print('FOR', ', '.join(forms))
-
-    new_line = re.sub('\([-\w .,]+?\)', '', new_line)
-
+    if examples or examples4:
+        print ('EX', examples + examples4)
+        t.write('EX ' + str(examples + examples4) + '\n')
 
     ### SYNONYM
     syn_contexts = re.findall('([-\w\(\) .!]*?)[;,:] ?(.+?)$', new_line)
@@ -140,53 +155,56 @@ for line in f:
     for context in syn_contexts:
         contexts = re.split('[;.]', syn_contexts[0][1])
         for cont in contexts:
-            cont = re.sub('(^ +| +$)', '', cont)
+            cont = re.sub('(^ +|[. ]+$)', '', cont)
             if ',' in cont:
-                cont_body += '<xr type="syn">\n<group>\n'+cont+'</group>\n</xr>\n'
+                cont_body += '<set>'+re.sub(' *, *', ', ', re.sub('[^-\w, ]', '', cont))+'</set>\n'
             elif ' ' in cont:
                 syns.append(cont)
+    if cont_body != '':
+        cont_body = '<xr type="synonym">\n' + cont_body + '</xr>\n'
 
     syn_body = ''
-    i = 1
     for syn in syns:
-        syn_body += '<ref n="'+str(i)+'">\n'+syn+'</ref>\n'
-        i += 1
+        syn_body += '<ref>'+re.sub('[><]', '', syn)+'</ref>\n'
     if syn_body != '':
-        syn_body = '<xr type="syn">\n'+syn_body+'</xr>\n'
-
-    # w.write('CON' + cont_body + '\n')
-    print('CON', syns)
+        syn_body = '<xr type="synonym">'+syn_body+'</xr>\n'
 
     new_line = re.sub('[;,:] ?(.+?)$', '', new_line)
 
+    if syn_contexts:
+        print('CON', syn_contexts)
+        t.write('CON ' + str(syn_contexts) + '\n')
 
     ### WORD
     input = re.findall('[-\w !.]+', new_line)
     word_body = ''
     if input != []:
         word = re.sub(' +$', '', input[0])
-        word_body += '<entry type="main">\n<form>\n<orth main="True">\n'+word+'</orth>\n</form>\n'
+        word_body += '<entry type="main">\n<index>'+str(num)+\
+                     '</index>\n<symbol>'+word[0]+'</symbol>\n<form>\n<orth type="lemma" extent="full">'+word+'</orth>\n</form>\n'
     new_line = re.sub('[-\w !.]+', '', new_line)
 
-    # w.write('W ' + word_body + '\n')
     print ('W', word)
+    t.write('W ' + str(word) + '\n')
 
-    # w.write('=' * 30 + '\n')
     print ('='*30)
+    t.write('='*30 + '\n')
+
+    num += 1
 
     body += word_body + \
               form_body + \
               '<sense n="1">\n' + \
               def_body + \
               ex_body + \
+              id_body + \
               sex_body + \
+              '</sense>\n' + \
               comp_body + \
               ref_body + \
               cont_body + \
               syn_body + \
               ant_body + \
-              '</sense>\n' + \
-              id_body + \
               '</entry>\n'
 
 
@@ -203,15 +221,13 @@ header = '<xml>\n' \
          '</fileDesc>\n' \
          '<front>\n' \
          '<head>\n' \
-         '<title volume="1" dict_id="">Словарь русских синонимов и сходных по смыслу выражений</title>\n' \
+         '<title dict_id="">Словарь русских синонимов и сходных по смыслу выражений</title>\n' \
          '<author>Наум Абрамович Переферкович</author>\n' \
          '<pubdate>1900</pubdate>\n' \
-         '</head>\n' \
-         '<dict_lang>\n' \
-         '<language n="content">\n' \
+         '<language type="content">\n' \
          '<idno type="iso639-3">rus</idno>\n' \
          '</language>\n' \
-         '</dict_lang>\n' \
+         '</head>\n' \
          '</front>\n' \
          '<body>\n' \
          '<div>\n' \
@@ -220,7 +236,7 @@ header = '<xml>\n' \
 footer = '</superEntry>\n' \
          '</div>\n' \
          '</body>\n' \
-         '<xml>\n'
+         '</xml>\n'
 
 main = header + body + footer
 w.write(main)
